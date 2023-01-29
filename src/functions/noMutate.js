fae65d76e6b5d08e32f6cbb321a21b1d656fd085
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2023-01-29 10:49:52 +0200

    Fix: to change the folder name

--- src/functions/noMutate.js
+++ src/functions/noMutate.js
@@ -1,4 +1,5 @@
-// use this function with spreading, because inner arrays and inner objects gets mutated
+// use this function with spreading or when an Object/array has inner arrays/objects, because inner
+// arrays and inner objects gets mutated
 // usage: const newObj = { ...noMutate(someObject) }
 // usage: const newArr = [...oldArr, noMutate(newItem)]
 const noMutate = (obj) => {
